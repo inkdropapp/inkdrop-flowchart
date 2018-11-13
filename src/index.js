431ef4ced0004d4d01b873c5e7d467bbe7caafe4
Takuya Matsuyama <nora@odoruinu.net>
2018-11-13 11:02:52 +0900

    fix(error): Show user-friendly error message

--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
+import PropTypes from 'prop-types'
 import { React } from 'inkdrop'
 import flowchart from 'flowchart.js'
 
 class Flowchart extends React.Component {
-  componentDidMount () {
+  static propTypes = {
+    children: PropTypes.node
+  }
+  componentDidMount() {
     this.renderDiagram()
   }
-  componentDidUpdate () {
+  componentDidUpdate() {
     this.renderDiagram()
   }
-  render () {
-    return <div ref='canvas' />
+  render() {
+    return <div className="markdown-flowchart" ref={el => (this.canvas = el)} />
   }
-  renderDiagram () {
-    const code = this.props.children[0]
-    const diagram = flowchart.parse(code)
-    this.refs.canvas.innerHTML = ''
-    diagram.drawSVG(this.refs.canvas)
+  renderDiagram() {
+    try {
+      const code = this.props.children[0]
+      const diagram = flowchart.parse(code)
+      this.canvas.innerHTML = ''
+      diagram.drawSVG(this.canvas)
+    } catch (error) {
+      this.canvas.innerHTML = `
+        <span class="ui error message mde-error-message">
+          <div class="header">Failed to render flowchart. Please check if the syntax is correct.</div>
+          <p>${error.message}</p>
+          <p><a href="https://flowchart.js.org/" target="_blank">How to write flowchart</a></p>
+        </span>
+      `
+    }
   }
 }
 
 module.exports = {
-  activate () {
+  activate() {
     const { MDEPreview } = inkdrop.components.classes
     if (MDEPreview) {
       MDEPreview.remarkCodeComponents.flowchart = Flowchart
     }
   },
 
-  deactivate () {
+  deactivate() {
     const { MDEPreview } = inkdrop.components.classes
     if (MDEPreview) {
       MDEPreview.remarkCodeComponents.flowchart = null
